234acf0305f4de1130546bd0833a004e0418d83d
Pello Usabiaga <pellousabiaga@gmail.com>
2026-05-16 10:18:31 +0200

    Block type identification moved out of library, now library should be minimal

--- src/BasicCppFactory.h
+++ src/BasicCppFactory.h
@@ -73,77 +73,6 @@ namespace BlockLibraries::BasicBlocksBasicCpp
                   throw std::out_of_range("Block type not found: " + blockClass);
             }
 
-            std::string ResolveSignalType(
-                  const std::vector<PySysLinkBase::PortTypeMetadata>& inputs,
-                  const std::vector<PySysLinkBase::PortTypeMetadata>& outputs)
-            {
-                  
-                  for (const auto& p : inputs)
-                  {
-                        if (p.category == PySysLinkBase::PortCategory::FullySupportedSignalValue &&
-                        p.signalValueType.has_value())
-                        {
-                              std::cout << "Resolved signal type from input port: " << p.signalValueType.value() << std::endl;
-                              return p.signalValueType.value();
-                        }
-                  }
-                  for (const auto& p : outputs)
-                  {
-                        if (p.category == PySysLinkBase::PortCategory::FullySupportedSignalValue &&
-                        p.signalValueType.has_value())
-                        {
-                              std::cout << "Resolved signal type from output port: " << p.signalValueType.value() << std::endl;
-                              return p.signalValueType.value();
-                        }
-                  }
-
-                  throw std::invalid_argument("Could not resolve signal type from ports.");
-            }
-
-            std::string PortCategoryToString(PySysLinkBase::PortCategory cat)
-            {
-                  using namespace PySysLinkBase;
-
-                  switch (cat)
-                  {
-                        case PortCategory::FullySupportedSignalValue: return "FullySupportedSignalValue";
-                        case PortCategory::Enumeration: return "Enumeration";
-                        case PortCategory::Structure: return "Structure";
-                        case PortCategory::PointerToObject: return "PointerToObject";
-                        case PortCategory::OtherType: return "OtherType";
-                        case PortCategory::Inherited: return "Inherited";
-                        case PortCategory::Unknown: return "Unknown";
-                        default: return "Invalid";
-                  }
-            }
-            std::string JoinPortTypes(const std::vector<PySysLinkBase::PortTypeMetadata>& types)
-            {
-                  if (types.empty())
-                        return "[]";
-
-                  std::ostringstream oss;
-                  oss << "[";
-
-                  for (size_t i = 0; i < types.size(); ++i)
-                  {
-                        oss << PortCategoryToString(types[i].category);
-                        oss << "(";
-                        if (types[i].signalValueType.has_value())
-                        {
-                              oss << types[i].signalValueType.value();
-                        }
-                        else
-                        {
-                              oss << "no signal value type";
-                        }
-                        oss << ")";
-                        if (i + 1 < types.size())
-                              oss << ", ";
-                  }
-
-                  oss << "]";
-                  return oss.str();
-            }
       public:
             std::vector<std::string> GetSupportedBlockClasses() const override
             {
@@ -161,32 +90,8 @@ namespace BlockLibraries::BasicBlocksBasicCpp
             
             std::unique_ptr<PySysLinkBase::ISimulationBlock> CreateBlock(std::string blockClass, std::map<std::string, PySysLinkBase::ConfigurationValue> blockConfiguration, 
                                                                          std::shared_ptr<PySysLinkBase::IBlockEventsHandler> blockEventsHandler,
-                                                                         int inputPortAmmount, int outputPortAmmount) override
+                                                                         std::string dataType, int inputPortAmmount, int outputPortAmmount) override
             {
-                  auto inputTypes = PySysLinkBase::ParsePortTypeMetadatas(
-                  PySysLinkBase::ConfigurationValueManager::TryGetConfigurationValue<std::vector<std::string>>(
-                        "InputPortTypes", blockConfiguration));
-
-                  auto outputTypes = PySysLinkBase::ParsePortTypeMetadatas(
-                  PySysLinkBase::ConfigurationValueManager::TryGetConfigurationValue<std::vector<std::string>>(
-                        "OutputPortTypes", blockConfiguration));
-                  
-                  std::string dataType;
-                  try {
-                        dataType = ResolveSignalType(inputTypes, outputTypes);
-                  }
-                  catch (const std::invalid_argument&)
-                  {
-                        throw std::invalid_argument(
-                              "Could not resolve signal type for block '" + blockClass +
-                              "'\n  InputPortTypes: " + JoinPortTypes(inputTypes) +
-                              "\n  OutputPortTypes: " + JoinPortTypes(outputTypes)
-                        );
-                  }
-
-
-                  std::cout << "Creating block of class " << blockClass << " with data type " << dataType << std::endl;
-
                   if (dataType == "int")
                   {
                         return CreateBlockTyped<int>(blockClass, blockConfiguration, blockEventsHandler, inputPortAmmount, outputPortAmmount);
